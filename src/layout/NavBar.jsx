8f0bb8d7f24273247c9381d718a2b6c6abeca585
James <james@smoothbook.co>
2017-10-03 14:18:15 +0700

    Update dependencies

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -2,13 +2,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default class Navleft extends React.Component{
+export default class NavBar extends React.Component{
 
     constructor(props){
 
         super(props);
-
-
 
     }
 
