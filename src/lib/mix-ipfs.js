83c0e0c86e50f8a000cd4affda7fd1b384eda287
James <james@smoothbook.co>
2017-10-04 14:38:23 +0700

    Start IPFS library

--- src/lib/mix-ipfs.js
+++ src/lib/mix-ipfs.js
@@ -1,13 +1,66 @@
 
 // Library for managing MIX interactions with IPFS
+const IPFS = require('ipfs'),
+    series = require('async').series;
 
 export default class MixIpfs{
 
     constructor(options = null){
 
+        this.ipfsNode = new IPFS();
+
+        let fileMultihash = null;
+
+        series(
+            [
+                (cb) => this.ipfsNode.on('ready', cb),
+                (cb) => this.ipfsNode.version((err, version) => {
+                    if (err) {
+                        return cb(err)
+                    }
+                    console.log('Version:', version.version);
+                    cb()
+                })
+            ]
+        );
+
     }
 
+    getNode(){
 
+        if(!this.ipfsNode) throw new Error('Node does not exist');
+
+        return this.ipfsNode;
+
+    }
+
+    addContent(){
+
+        const content =  {
+            path: 'hello.txt',
+            content: Buffer.from('Elloo der')
+        };
+
+        return new Promise(
+            (resolve, reject)=>{
+
+                this.ipfsNode.files.add(content, (err, result) => {
+
+                    if (err) { return reject(err); }
+
+                    let fileMultiHash = null;
+
+                    console.log('\nAdded file:', result[0].path, result[0].hash);
+
+                    fileMultihash = result[0].hash;
+
+                    resolve(fileMultiHash);
+                })
+
+            }
+        )
+
+    }
 
 }
 
--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -2,9 +2,7 @@
 
 import React from 'react';
 
-const IPFS = require('ipfs'),
-    node = new IPFS(),
-    series = require('async').series;
+import MixIpfs from '../lib/mix-ipfs';
 
 export default class Publish extends React.Component {
 
@@ -12,37 +10,12 @@ export default class Publish extends React.Component {
 
         super(props);
 
-        let fileMultihash = null;
-
-        series(
-            [
-                (cb) => node.on('ready', cb),
-                (cb) => node.version((err, version) => {
-                    if (err) {
-                        return cb(err)
-                    }
-                    console.log('Version:', version.version);
-                    cb()
-                }),
-                (cb) => node.files.add({
-                    path: 'hello.txt',
-                    content: Buffer.from('Elloo der')
-                }, (err, result) => {
-                    if (err) { return cb(err) }
-
-                    // Once the file is added, we get back an object containing the path, the
-                    // multihash and the sie of the file
-                    console.log('\nAdded file:', result[0].path, result[0].hash);
-                    fileMultihash = result[0].hash;
-                    // cb()
-                })
-            ]
-        )
 
     }
 
     componentDidMount() {
 
+        this.ipfs = new MixIpfs();
 
     }
 
